22157630feb2042f7cabb1927d6794dc29b0a788
Yasir Khan <yasirkhanone22@gmail.com>
2024-09-16 23:47:19 +0500

    Refactor code for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,24 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://astro-nano-demo.vercel.app",
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: "houston",
+      langs: [
+        "tsx",
+        "javascript",
+        "typescript",
+        "jsx",
+        "json",
+        "css",
+        "html",
+        "markdown",
+        "yaml",
+        "python",
+      ],
+      wrap: true,
+      lineNumbers: true,
+      transformers: [],
+    },
+  },
 });
